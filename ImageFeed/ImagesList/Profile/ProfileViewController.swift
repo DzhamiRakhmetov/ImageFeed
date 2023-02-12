b5990a19531c141a14c9e2b4887548cbb412ba92
Dzhami <rakhmetov.dzhami@yandex.ru>
2023-02-12 17:02:36 +0700

    .

--- ImageFeed/ImagesList/Profile/ProfileViewController.swift
+++ ImageFeed/ImagesList/Profile/ProfileViewController.swift
@@ -8,12 +8,9 @@
 import UIKit
 
 final class ProfileViewController : UIViewController {
-    // 
-    private lazy var profileImage : UIImage = {
-        let profileImage = UIImage(named: "avatar")!
-        return profileImage
-    }()
     
+    private let profileImage = UIImage(named: "avatar")
+        
     private lazy var imageView : UIImageView = {
         let imageView = UIImageView(image: profileImage)
         imageView.translatesAutoresizingMaskIntoConstraints = false
